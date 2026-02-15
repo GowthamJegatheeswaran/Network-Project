e0b59a6db7082b6ba0de3b2c30a5f8ee7841ab0b
Jegatheeswaran Gowtham <gowtham@Jegatheeswarans-MacBook-Air.local>
2026-02-15 19:10:19 +0530

    last

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    },
+    transports: ["websocket", "polling"]
+});
 
 app.use(express.static("public"));
 
